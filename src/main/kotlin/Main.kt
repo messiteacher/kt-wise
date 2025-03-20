11487c278a389158bceacffb081bbc1b34229f55
ShinYounHo <suh2963@gmail.com>
2025-03-20 15:19:37 +0900

    work1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,13 @@
-fun main(args: Array<String>) {
+fun main() {
 
+    println("== 명언 앱 ==")
+    while (true) {
+
+        print("명령) ")
+        val input = readlnOrNull() ?: ""
+
+        if (input == "종료") {
+            break;
+        }
+    }
 }
