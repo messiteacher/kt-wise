7ba3d028be79eb19a82463debd43e93f20309abb
ShinYounHo <suh2963@gmail.com>
2025-03-21 10:18:22 +0900

    app class apply

---
+++ src/main/kotlin/App.kt
@@ -0,0 +1,35 @@
+import domain.system.controller.SystemController
+import domain.wiseSaying.controller.WiseSayingController
+import global.Request
+
+class App {
+
+    fun run() {
+
+        val wiseSayingController = WiseSayingController()
+        val systemController = SystemController()
+
+        println("== 명언 앱 ==")
+        while (true) {
+
+            print("명령) ")
+            val input = readlnOrNull() ?: ""
+
+            val rq = Request(input)
+
+            when (rq.actionName) {
+
+                "종료" -> {
+                    systemController.exit()
+                    break
+                }
+                "등록" -> wiseSayingController.write()
+                "목록" -> wiseSayingController.list()
+                "삭제" -> wiseSayingController.delete(rq)
+                else -> {
+                    println("알 수 없는 명령입니다.")
+                }
+            }
+        }
+    }
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,33 +1,7 @@
 
-import domain.system.controller.SystemController
-import domain.wiseSaying.controller.WiseSayingController
-import global.Request
 
 fun main() {
 
-    val wiseSayingController = WiseSayingController()
-    val systemController= SystemController()
-
-    println("== 명언 앱 ==")
-    while (true) {
-
-        print("명령) ")
-        val input = readlnOrNull() ?: ""
-        val rq = Request(input)
-
-        when (rq.actionName) {
-
-            "종료" -> {
-                systemController.exit()
-                break
-            }
-            "등록" -> wiseSayingController.write()
-            "목록" -> wiseSayingController.list()
-            "삭제" -> wiseSayingController.delete(rq)
-
-            else -> {
-                println("알 수 없는 명령입니다.")
-            }
-        }
-    }
+    val app = App()
+    app.run()
 }
