731d0fdf5743464ef07a16b41e31dc31907ae593
ShinYounHo <suh2963@gmail.com>
2025-03-21 12:36:47 +0900

    list test

--- src/test/kotlin/AppTest.kt
+++ src/test/kotlin/AppTest.kt
@@ -18,4 +18,24 @@ class AppTest {
         assertThat(out).contains("작가:")
         assertThat(out).contains("1번 명언이 등록되었습니다.")
     }
+
+    @Test
+    fun `명언 목록`() {
+        val result = TestBot.run(
+            """
+             등록
+             나의 죽음을 적들에게 알리지 말라.
+             충무공 이순신
+             등록
+             천재는 99%의 노력과 1%의 영감이다.
+             에디슨
+             목록
+         """
+        )
+
+        println(result)
+
+        assertThat(result).contains("1 / 충무공 이순신 / 나의 죽음을 적들에게 알리지 말라.")
+        assertThat(result).contains("2 / 에디슨 / 천재는 99%의 노력과 1%의 영감이다.")
+    }
 }
