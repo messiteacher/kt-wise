cf4b083a94a5cc0d380d0af23be95d06ac0047e8
ShinYounHo <suh2963@gmail.com>
2025-03-21 14:15:15 +0900

    singleton lazy

--- src/main/kotlin/global/SingletonScope.kt
+++ src/main/kotlin/global/SingletonScope.kt
@@ -7,8 +7,8 @@ import domain.wiseSaying.service.WiseSayingService
 
 object SingletonScope {
 
-    val wiseSayingRepository = WiseSayingRepository()
-    val wiseSayingService = WiseSayingService()
-    val wiseSayingController = WiseSayingController()
-    val systemController = SystemController()
+    val wiseSayingRepository by lazy { WiseSayingRepository() }
+    val wiseSayingService by lazy { WiseSayingService() }
+    val wiseSayingController by lazy { WiseSayingController() }
+    val systemController by lazy { SystemController() }
 }
