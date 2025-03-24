3412a53eae2542e8f2546380a81d47328367a68e
ShinYounHo <suh2963@gmail.com>
2025-03-24 11:37:38 +0900

    makeSampleData

--- src/test/kotlin/TestBot.kt
+++ src/test/kotlin/TestBot.kt
@@ -9,6 +9,15 @@ class TestBot {
         val originalIn = System.`in` // 표준 입력 - 키보드
         val originalOut = System.`out` // 표준 출력 - 모니터
 
+        fun makeSampleData(size: Int) {
+
+            val input = (1..size).joinToString("\n") { i ->
+                "등록\n명언 ${i}\n작자미상 ${i}\n"
+            }
+
+            run(input)
+        }
+
         fun run(input: String): String {
 
             val formattedInput = input.trimIndent().plus("\n종료")
